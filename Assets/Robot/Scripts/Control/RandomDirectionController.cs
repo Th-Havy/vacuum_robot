6f470bb8884e5061a5357ebb92e50f8320470baa
Th-Havy <thomashavy@hotmail.fr>
2022-10-08 08:19:19 +0200

    finished implementing keyboard teleoperation

--- Assets/Robot/Scripts/Control/RandomDirectionController.cs
+++ Assets/Robot/Scripts/Control/RandomDirectionController.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 [RequireComponent(typeof(LowLevelController))]
 public class RandomDirectionController : MonoBehaviour
 {
+    /// <summary>
+    /// The robot goes forward until a collision occurs, then goes backward for a bit
+    /// and finally turn at a random angle before going forward again.
+    /// </summary>
+    enum State
+    {
+        Forward,
+        Backward,
+        Turn
+    }
     public float linearVelocity = 0.4f;
     public float angularVelocity = 1.0f;
-    
+    public float backwardDistance = 0.1f;
+
     private LowLevelController _controller;
+
+    private State _state = State.Forward;
 
     void Start()
     {
         _controller = GetComponent<LowLevelController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // TODO: read fall prevention sensors to stop forward/backward motion
+        // or keep turning until there is no more fall in front
+        switch (_state)
+        {
+            case State.Forward:
+                _controller.LinearVelocity = linearVelocity;
+                _controller.AngularVelocity = 0f;
+                break;
+            case State.Backward:
+                _controller.LinearVelocity = -linearVelocity;
+                _controller.AngularVelocity = 0f;
+                break;
+            case State.Turn:
+                _controller.LinearVelocity = 0f;
+                _controller.AngularVelocity = angularVelocity;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void HandleBaseCollision(RosMessageTypes.Std.Float32Msg message)
+    {
+
     }
 }
